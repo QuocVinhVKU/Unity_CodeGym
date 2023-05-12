177567e4f7eeac11b1d2e787527be953b815e595
QuocVinhVKU <kuroblackshirowhile@gmail.com>
2023-05-13 00:29:04 +0700

    update B1 C# co ban

---
+++ Bai1_C#canban/CurrencyConversion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CurrencyConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float rate = 23000f;
+            float usd;
+            Console.Write("Enter USD: ");
+            usd = float.Parse(Console.ReadLine());
+            Console.WriteLine("VND is: " + (rate * usd));
+        }
+    }
+}
---
+++ Bai1_C#canban/HienThiLoiChao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HienThiLoiChao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
