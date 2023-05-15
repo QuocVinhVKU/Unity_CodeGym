bdd4007c921660e749afe2ae281f7444b975915b
QuocVinhVKU <kuroblackshirowhile@gmail.com>
2023-05-15 13:59:43 +0700

    push

--- Bai1_C#canban/SystemTime/Program.cs
+++ Bai1_C#canban/SystemTime/Program.cs
@@ -7,7 +7,8 @@ namespace SystemTime
         static void Main(string[] args)
         {
             DateTime localDate = DateTime.Now;
-            System.Console.WriteLine("Datetime Now is :" + localDate);
+            System.Console.WriteLine("Datetime Now is :" + localDate.ToString("dd/MM/yyyy hh:mm tt"));
+            
         }
     }
 }
